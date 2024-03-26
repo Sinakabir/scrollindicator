b33942a1c0277a90d1776da431c82ec086c533a1
Sina Persian <sinasasy4@gmail.com>
2024-03-25 17:55:53 -0700

    Scroll-Indicator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Scrollindicator from './components/scroll-indicator/Scrollindicator'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Scrollindicator url={'https://dummyjson.com/products?limit=100'}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
